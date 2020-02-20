be05af639ded2e1be7be51a335137466e674e74c
charlescatta <charles.catta@gmail.com>
2020-02-20 18:54:08 +0000

    Deploying to gh-pages from pages - 63520b54c05a5b1a56812d26ccdea4c0348c88f0 🚀

---
+++ precache-manifest.ba5901e2db2f647ea3f2edbab1f15e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79e7d3e96245b35e8f8d",
+    "url": "/static/css/main.cb718d12.chunk.css"
+  },
+  {
+    "revision": "79e7d3e96245b35e8f8d",
+    "url": "/static/js/main.17fc5e72.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "460053ad9d3a46d3b63c",
+    "url": "/static/css/2.aeb43374.chunk.css"
+  },
+  {
+    "revision": "460053ad9d3a46d3b63c",
+    "url": "/static/js/2.c68a047b.chunk.js"
+  },
+  {
+    "revision": "269d14b3b0fedcbf93df825f6389e6c0",
+    "url": "/index.html"
+  }
+];
